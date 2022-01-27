351ed861793442a356f346d46d8a4bb8bd3ee65b
Kent Pedersen <webzone@kpdesign.dk>
2022-01-27 12:37:51 +0100

    Fixed the delete function to use params instead of body to find the index

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.post('/postdictators', (req, res) => {
     const dictator = req.body;
     dictators.push(dictator);
+    dictators[dictators.length - 1].index = dictators.length - 1;
     console.log(dictator);
 });
 
@@ -23,12 +24,12 @@ app.get('/getdictators', (req, res) => {
     console.log(dictators);
 });
 
-//deleting a specific dictator in the array
-app.delete('/deletedictators', (req, res) => {
-    const dictator = req.body.index;
-    dictators.splice(dictator, 1);
-    console.log(dictators);
-
+//deleting a specific dictator in the array by finding the right index based on the params in adress
+app.delete('/deletedictators/:index', (req, res) => {
+    const { index } = req.params;
+    const dictatorIndex = dictators.findIndex(p => p.index == index)
+    dictators.splice(dictatorIndex, 1);
+    return res.send();
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
